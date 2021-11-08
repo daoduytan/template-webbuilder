41a4d69de2ff0ca426a950647d25bd99da8b72f1
daoduytan <ddtan2212@gmail.com>
2021-11-08 11:13:35 +0700

    update

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const { Liquid } = require("liquidjs");
+const path = require("path");
 
 const app = express();
 const engine = new Liquid({
@@ -7,12 +8,22 @@ const engine = new Liquid({
   extname: ".liquid",
 });
 
+app.use(express.static("assets"));
 app.engine("liquid", engine.express()); // register liquid engine
-app.set("views", ["./partials", "./views"]); // specify the views directory
+app.set("views", ["./theme", "./partials", "./templates", "./views"]); // specify the views directory
+// app.set("views", ["./templates", "./views"]); // specify the views directory
 app.set("view engine", "liquid"); // set to default
 
 app.get("/", function (req, res) {
-  res.render("index");
+  res.render("index", {
+    title: "Homepage",
+  });
+});
+
+app.get("*", function (req, res) {
+  res.render("404", {
+    title: "404",
+  });
 });
 
 app.get("/todos", function (req, res) {
